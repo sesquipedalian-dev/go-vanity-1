5dfb57f736fea4e21daeff466cda2ad2567aa31a
sesquipedalian-dev <scott.agnew@checkr.com>
2021-03-17 15:46:30 -0600

    Add func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sesquipedalian-dev.github.io/go-vanity-1
+
+go 1.15
---
+++ greetings.go
@@ -0,0 +1,8 @@
+package go_vanity_1
+
+import "fmt"
+
+func Hello(name string) string { 
+	return fmt.Sprintf("Hi, %v. Welcome!", name)
+}
+
